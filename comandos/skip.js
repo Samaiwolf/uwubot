1aa01e143f1d1e803650ef34a1ce0520e3c96b7a
Samai <matiaslobos12@hotmail.com>
2021-09-23 05:36:57 -0300

    V 0.2.1 Agregue el comando skip y puli el comando leave, bugfix

--- comandos/skip.js
+++ comandos/skip.js
@@ -2,22 +2,21 @@ const Discord = require('discord.js');
 const distube = require('distube');
 
 module.exports = {
-  name: "stop",
-  alias: [],
+  name: "skip",
+  alias: ["next"],
 
 execute (client, message, args){
-
+    
     if(!message.member.voice.channel) return message.channel.send("Debes estar en un canal de voz")
     if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id)
     return message.channel.send("Debes estar en el mismo canal que yo")
 
     const serverQueue = client.distube.getQueue(message)
-
     if(!serverQueue) return message.channel.send("No hay canción reproduciendose ahora")
 
+    client.distube.skip(message)
+    message.channel.send("Salteo la cancion")
 
-    client.distube.stop(message)
-    message.channel.send("Salio el bot")
  }
 
 }
